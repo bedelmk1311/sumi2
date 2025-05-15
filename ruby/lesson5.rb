203aa8551ea5548066f9509b230923a7fbe5b464
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-05-15 08:34:54 +0000

    条件分

---
+++ ruby/lesson5.rb
@@ -0,0 +1,18 @@
+total_money = 101
+
+if total_money > 100
+  puts "みかんを購入。所持金に余りあり。"
+elsif total_money == 100
+  puts "みかんを購入。所持金は0円。"
+else total_money < 100 
+  puts "みかんを購入することができません。"
+end
+
+# 解答　
+# if total_money > 100
+#   puts "みかんを購入。所持金に余りあり。"
+# elsif total_money == 100
+#   puts "みかんを購入。所持金は0円。"
+# else
+#   puts "みかんを購入することができません。"
+# end
