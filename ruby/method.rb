5fc2d2f4149f5322cf253358b8c9c41cbaa3581b
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-05-17 14:17:34 +0000

    methods

---
+++ ruby/method.rb
@@ -0,0 +1,15 @@
+# length
+# 文字列型に使用すると文字数を数えてくれるlengthメソッド
+puts "WEBCAMP".length
+#7 と出る
+
+#reverse
+# 文字列を逆に表示するreverseメソッド
+puts "WEBCAMP".reverse
+# PMACBEW と出る
+
+# include?
+# 引数に指定した文字列が含まれているかどうかを調べるinclude?メソッド
+puts "WEBCAMPでプログラミング学習".include?("WEBCAMP")
+# true と出る
+
