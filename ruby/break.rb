5499f470137adfa918960a293a07c60998c2f749
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-05-17 09:37:16 +0000

    break

---
+++ ruby/break.rb
@@ -0,0 +1,13 @@
+# 繰り返し処理を中断するには、「break」を使います。
+# breakは、while、for、eachなどの繰り返し文の中で利用します。
+
+i = 1
+
+while i <= 10 do
+  if i == 5
+    puts "処理を終了します"
+    break
+  end
+  puts i
+  i += 1 # i = i + 1と同じ
+end
