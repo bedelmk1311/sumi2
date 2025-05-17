084b6ec176668daed49484054274336135bf9c95
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-05-17 10:08:58 +0000

    [middle]lesson6-2

---
+++ ruby/lesson6-2.rb
@@ -0,0 +1,25 @@
+i = 1
+puts "計算を始めます"
+puts "何回繰り返しますか？"
+n = gets.to_i
+
+while i <= n do
+  if i == n 
+    puts "すべての計算を終了します"
+    break 
+  end
+  puts "#{n}回目の計算"
+  puts "２つの値を入力してください"
+  x = gets.to_i
+  y = gets.to_i
+  puts "入力した数字は#{x}と#{y}です"
+
+  puts "計算を始めます"
+  puts  "足し算は#{x}+#{y}=#{x + y}"
+  puts  "引き算は#{x}-#{y}=#{x - y}"
+  puts  "掛け算は#{x}*#{y}=#{x * y}"
+  puts  "割り算は#{x}/#{y}=#{x / y}"
+
+  puts "計算を終了します"
+  i = i +1
+end
