d3b3e4b85993e579d64ffbe5238a058db88553ba
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-05-17 14:58:33 +0000

    fizzbuzz

--- ruby/fizz_buzz.rb
+++ ruby/fizz_buzz.rb
@@ -6,9 +6,17 @@ puts "FizzBuzzは3で割り切れる数値を引数に渡すと、「Fizz」、
 puts "好きな数字を入力してください"
 a = gets.to_i
 
-
-
-
+def FizzBuzz(a)
+  if a % 15 == 0
+    puts "FizzBuzz"
+  elsif a % 5 == 0
+    puts "Buzz"
+  elsif a % 3 == 0
+    puts "Fizz"
+  else
+    puts "#{a}は割り切れないです"
+  end
+end
 
 puts "#{a}の結果は..."
-puts ""
+puts FizzBuzz(a)
