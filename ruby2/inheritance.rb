a46ce75141c8b799ee5b1f0d5c65fa01944edf7c
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-05-28 14:56:20 +0000

    superclass

--- ruby2/inheritance.rb
+++ ruby2/inheritance.rb
@@ -1,14 +1,34 @@
+# class Car
+#   def run(distance)
+#     puts "車で#{distance}キロ走ります。"
+#   end
+# end
+
+# class Bus
+#   def run(distance)
+#     puts "車で#{distance}キロ走ります。"
+#   end
+# end
+
+# bus = Bus.new
+# bus.run(5)
+
+
+# def run(distance)
+#   puts "車で#{distance}キロ走ります。"
+# end
+# が重複している時に継承を使う
+
 class Car
   def run(distance)
     puts "車で#{distance}キロ走ります。"
   end
 end
 
-class Bus
-  def run(distance)
-    puts "車で#{distance}キロ走ります。"
-  end
+class Bus < Car
 end
 
 bus = Bus.new
 bus.run(5)
+
+# 車で5キロ走ります。
